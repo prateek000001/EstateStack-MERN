f8f92ee731e5a6d133a3e6a5a9a0caba30ec9bb7
Prateek Lahari <prateeklahari10@gmail.com>
2025-06-11 16:24:22 +0530

    update the header and make the profile page private...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -14,8 +15,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} /> {/* Fixed path */}
-        <Route path="/profile" element={<Profile />} /> {/* Added slash */}
         <Route path="/about" element={<About />} />     {/* Added slash */}
+         <Route element={<PrivateRoute />} />
+        <Route path="/profile" element={<Profile />} /> {/* Added slash */}
       </Routes>
     </BrowserRouter>
   );
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "estatestake-mern.firebaseapp.com",
@@ -17,5 +13,13 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
- export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const app = initializeApp(firebaseConfig);
+
+// Load analytics only in production
+if (import.meta.env.PROD) {
+  import("firebase/analytics").then(({ getAnalytics }) => {
+    getAnalytics(app);
+  }).catch((err) => {
+    console.warn("Analytics failed to load:", err);
+  });
+}
