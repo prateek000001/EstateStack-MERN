02a4fedfb82b7850df08ed4a935174eae7106650
Prateek Lahari <prateeklahari10@gmail.com>
2025-06-24 00:25:10 +0530

    compete imageSlider and listing page.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} /> {/* Fixed path */}
         <Route path="/about" element={<About />} />     {/* Added slash */}
+         <Route path='/listing/:listingId' element={<Listing />} />
          <Route element={<PrivateRoute />} />
         <Route path="/profile" element={<Profile />} /> {/* Added slash */}
        <Route path='/create-listing' element={<CreateListing />} />
