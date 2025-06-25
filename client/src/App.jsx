5e8e78b51f2ca96223339f1cd0fa59b50d7d4488
Prateek Lahari <prateeklahari10@gmail.com>
2025-06-26 00:43:18 +0530

    complete Search UI with onChange and onSubmit functionality..

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 export default function App() {
   return (
@@ -20,6 +21,7 @@ export default function App() {
         <Route path="/sign-up" element={<SignUp />} /> {/* Fixed path */}
         <Route path="/about" element={<About />} />     {/* Added slash */}
          <Route path='/listing/:listingId' element={<Listing />} />
+         <Route path='/search' element={<Search />} />
          <Route element={<PrivateRoute />} />
         <Route path="/profile" element={<Profile />} /> {/* Added slash */}
        <Route path='/create-listing' element={<CreateListing />} />
